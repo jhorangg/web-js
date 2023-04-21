93f52f40c791dc20e0ad02d16caca546b8826bbb
jhoan reyes <gitzzzgg@gmail.com>
2023-04-21 11:20:41 -0500

    act.2

---
+++ Cambiar imagen/app.js
@@ -0,0 +1,10 @@
+const uno=()=>{
+    document.querySelector("#img").src="134.jpg"
+}
+
+const dos=()=>{
+    document.querySelector("#img").src="mariana.jpg"
+}
+const tres=()=>{
+    document.querySelector("#img").src="rivers.jpg"
+}
